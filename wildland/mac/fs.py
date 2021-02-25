061fe11a5f03d46531370dcd7c81122d16db0d44
Piotr K. Isajew <pki@ex.com.pl>
2021-02-25 12:59:41 +0100

    wip: prepare to start control server

--- wildland/mac/fs.py
+++ wildland/mac/fs.py
@@ -7,6 +7,8 @@
 
 import logging
 from .apple_log import apple_log
+from ..control_server import ControlServer, ControlHandler, control_command
+from ..manifest.schema import Schema
 
 logger = logging.getLogger('fs')
 
@@ -16,12 +18,44 @@ class WildlandAbstractFS:
     than assuming speficic filesystem driver (i.e. FUSE) 
     '''
 
+    def __init__(self, socket_path):
+        # Mount information
+        self.storages: Dict[int, StorageBackend] = {}
+        self.storage_extra: Dict[int, Dict] = {}
+        self.storage_paths: Dict[int, List[PurePosixPath]] = {}
+        self.main_paths: Dict[PurePosixPath, int] = {}
+        self.storage_counter = 1
+        self.multithreaded = true
+        self.mount_lock = threading.Lock()
 
+        self.control_server = ControlServer()
+        self.control_server.register_commands(self)
+        self.default_user = None
+        self.uid = None
+        self.gid = None
+        self.socket_path = socket_path
+        command_schemas = Schema.load_dict('commands.json', 'args')
+        self.control_server.register_validators({
+            cmd: schema.validate for cmd, schema in command_schemas.items()
+        })
     
     def start(self):
         apple_log.configure()
-        logger.info('Wildland is starting')
+        self.uid, self.gid = os.getuid(), os.getgid()
+        logger.info('Wildland is starting, control socket: %s',
+                        self.socket_path)
+        self.control_server.start(self.socket_path)
 
+    @control_command('status')
+    def control_status(self, _handler):
+        """
+        Status of the control client, returns a dict with parameters; currently only
+        supports default (default_user)
+        """
+        logger.debug('got control command: status')
+        result = dict()
+        result['default-user'] = self.default_user
+        return result
 
 def main():
     server = WildlandAbstractFS()
