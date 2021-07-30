1c8866c394259cdc826e61555096a42be5b76b06
Michał Kluczek <michal@wildland.io>
2021-07-30 17:10:09 +0200

    Satisfy mypy and pylint

--- wildland/manifest/template.py
+++ wildland/manifest/template.py
@@ -239,6 +239,9 @@ class TemplateManager:
                 except TemplateError:
                     logger.warning('failed to load template file %s', file)
                     continue
+
+        templates.sort(key=str)
+
         return templates
 
     def get_template_file_by_name(self, template_name: str) -> TemplateFile:
