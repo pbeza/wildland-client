e6e7c43c4625a2ae708fa4d3277ffb1ea21b61b8
Michał Haponiuk <mhaponiuk@wildland.io>
2021-10-26 19:26:30 +0000

    WIP

--- wildland/md_pdf/renderer.py
+++ wildland/md_pdf/renderer.py
@@ -1,9 +1,13 @@
-from typing import BinaryIO, Iterable, List, Optional
+from typing import BinaryIO, DefaultDict, Iterable, List, Optional
 from wildland.md_pdf.traveller import TravellerBase
 from pathlib import Path
 from markdown import markdown
 from io import BytesIO
 from weasyprint import HTML
+from collections import defaultdict
+from anytree import Node, RenderTree
+from anytree.search import findall
+from anytree.resolver import Resolver, ChildResolverError
 
 
 class Engine:
@@ -29,8 +33,6 @@ class RendererBase:
     def run(self) -> None:
         ...
 
-    
-
 
 class BasicRenderer(RendererBase):
     def __init__(self, traveller: TravellerBase, engine=Engine):
@@ -46,17 +48,66 @@ class BasicRenderer(RendererBase):
                 markdowns.append('\n')
         squashed = "\n".join(markdowns)
         return squashed
-        
+
+    @staticmethod
+    def _node_fullpath(node: Node) -> str:
+        """
+        from Node('/root/home/user/wildland/gitlab/cargo/file.md')
+        to
+        '/root/home/user/wildland/gitlab/cargo/file.md'
+        """
+        return "/" + node.separator.join([n.name for n in reversed(list(node.iter_path_reverse()))])
+
+    @staticmethod
+    def _find_by_fullpath(root: Node, fullpath: str) -> Optional[Node]:
+        r = Resolver('name')
+        try:
+            node = r.get(root, fullpath)
+            return node
+        except ChildResolverError:
+            return None
+
+    def _start_path_node(self, root: Node) -> Node:
+        """
+        start_path in traveller term
+        """
+        node = BasicRenderer._find_by_fullpath(root, traveller.start_path)
+        assert node is not None
+        return node
+
+    @staticmethod
+    def _recursive_make_tree(dirs: List[str], parent: Node, root: Node) -> Node:
+        name = dirs[0]
+        fullname = root.separator.join([BasicRenderer._node_fullpath(parent), name])
+        child = BasicRenderer._find_by_fullpath(root, fullname)
+        if child == None:
+            child = Node(name, parent=parent)
+        if len(dirs) == 1:
+            return child
+        return BasicRenderer._recursive_make_tree(dirs[1:], child, root)
+
+    def make_tree(self, paths: Iterable[Path]) -> Node:
+        root = Node('#')
+        for path in paths:
+            dirs: List[str] = str(path).split("/")[1:]
+            BasicRenderer._recursive_make_tree(dirs, parent=root, root=root)
+        # print(RenderTree(root))
+
+        # trim from root to start_path dir
+        r = Resolver('name')
+        node = r.get(root, "/#" + str(self.traveller.start_path))
+        return node
+
     def run(self) -> None:
-        files = list(self.traveller.run())
-        # todo group into 1 pdf
-
+        paths = list(self.traveller.run())
+        node = self.make_tree(paths)
+        print(RenderTree(node))
+        
         # fix me files[:2]
-        squashed = self.squash_markdowns(files[:2])
+        squashed = self.squash_markdowns(paths[:2])
         stream: BytesIO = self.engine.render(squashed)
-
         # todo write_to file
-        print(squashed)
+        # print(squashed)
 
 
 if __name__ == "__main__":
