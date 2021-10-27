5ec0c2b9c322c80a11caeb703574cf1953be5002
Michał Haponiuk <mhaponiuk@wildland.io>
2021-10-27 12:52:07 +0000

    refactor

--- wildland/md_pdf/renderer.py
+++ wildland/md_pdf/renderer.py
@@ -1,19 +1,17 @@
-from typing import BinaryIO, DefaultDict, Iterable, List, Optional
-from wildland.md_pdf.traveller import TravellerBase
-from pathlib import Path
-from markdown import markdown
 from io import BytesIO
+from pathlib import Path
+from typing import Iterable, List, Optional
+
+from anytree import Node, PreOrderIter, RenderTree
+from anytree.resolver import ChildResolverError, Resolver
+from markdown import markdown
 from weasyprint import HTML
-from collections import defaultdict
-from anytree import Node, RenderTree, PreOrderIter
-from anytree.search import findall
-from anytree.resolver import Resolver, ChildResolverError
 
 
 class Engine:
     @staticmethod
     def render(text: str) -> BytesIO:
-        html = markdown(text, output_format='html4')
+        html = markdown(text, output_format='html')
         stream = BytesIO()
         # HTML(string=html).write_pdf("/home/user/wildland-client/pdf.pdf")
         HTML(string=html).write_pdf(stream)
@@ -21,61 +19,54 @@ class Engine:
 
     @staticmethod
     def render_to_file(text: str, path: Path) -> None:
-        html = markdown(text, output_format='html4')
+        html = markdown(text, output_format='html')
         HTML(string=html).write_pdf(path)
 
 
-class RendererBase:
-    def __init__(self, traveller: TravellerBase, engine=Engine()):
-        self.engine: Engine = engine
-        self.traveller = traveller
+class MarkdownRenderer:
+    def __init__(self, read_start_dir: Path, write_start_dir: Path):
+        self.read_start_dir = read_start_dir
+        self.write_start_dir = write_start_dir
+        self.engine = Engine()
 
-    def run(self) -> None:
-        ...
+    def find_markdowns(self) -> Iterable[Path]:
+        return self.read_start_dir.glob("**/*.md")
 
-
-class BasicRenderer(RendererBase):
-    def __init__(self, traveller: TravellerBase, engine=Engine()):
-        super().__init__(traveller, engine)
-
-    def squash_markdowns(self, paths: Iterable[Path]) -> str:
+    @staticmethod
+    def squash_markdowns(paths: Iterable[Path]) -> str:
         markdowns: List[str] = []
         for n, path in enumerate(paths, start=1):
             print(f"squashing {n}th markdown")
             title_header = f"# {path.name.strip('.md')}\n"
+            markdowns.append(title_header)
             with path.open("rt") as f:
-                markdowns.append(title_header)
                 markdowns.append(f.read())
-                markdowns.append('\n')
+            markdowns.append('\n')
         squashed = "\n".join(markdowns)
         return squashed
 
     @staticmethod
     def _node_fullpath(node: Node) -> str:
         """
-        from Node('/root/home/user/wildland/gitlab/cargo/file.md')
-        to
-        '/root/home/user/wildland/gitlab/cargo/file.md'
+        example
+        args node=Node('/#/home/user/wildland/gitlab/cargo/file.md')
+        returns '/#/home/user/wildland/gitlab/cargo/file.md'
         """
         return "/" + node.separator.join([n.name for n in reversed(list(node.iter_path_reverse()))])
 
     @staticmethod
-    def _find_by_fullpath(node: Node, fullpath: str) -> Optional[Node]:
+    def _find_node_by_fullpath(node: Node, fullpath: str) -> Optional[Node]:
+        """
+        example
+        args node=Node('/#'), fullpath='/#/home/user/wildland/gitlab/cargo'
+        returns Node('/#/home/user/wildland/gitlab/cargo') or None if does not exist
+        """
         r = Resolver('name')
         try:
             node = r.get(node, fullpath)
             return node
         except ChildResolverError:
             return None
-
-    @staticmethod
-    def _start_path_node(root: Node, start_path_tree_str: str) -> Node:
-        """
-        start_path in traveller term
-        """
-        node = BasicRenderer._find_by_fullpath(root, start_path_tree_str)
-        assert node is not None
-        return node
 
     @staticmethod
     def detach_from_parent(node: Node):
@@ -85,29 +76,30 @@ class BasicRenderer(RendererBase):
     @staticmethod
     def _recursive_make_tree(dirs: List[str], parent: Node, root: Node, markdown_path: Path) -> Node:
         name = dirs[0]
-        fullname = root.separator.join([BasicRenderer._node_fullpath(parent), name])
-        child = BasicRenderer._find_by_fullpath(root, fullname)
+        fullname = root.separator.join([MarkdownRenderer._node_fullpath(parent), name])
+        child = MarkdownRenderer._find_node_by_fullpath(root, fullname)
         if child is None:
             child = Node(name, parent=parent)
         if len(dirs) == 1:
             child.markdown_path = markdown_path
             return child
-        return BasicRenderer._recursive_make_tree(dirs[1:], child, root, markdown_path)
+        return MarkdownRenderer._recursive_make_tree(dirs[1:], child, root, markdown_path)
 
     def make_tree(self, paths: Iterable[Path]) -> Node:
         root = Node('#')
         for path in paths:
             dirs: List[str] = str(path).split("/")[1:]
-            BasicRenderer._recursive_make_tree(dirs, parent=root, root=root, markdown_path=path)
+            MarkdownRenderer._recursive_make_tree(dirs, parent=root, root=root, markdown_path=path)
         # print(RenderTree(root))
 
-        start_path_tree_str =  "/#" + str(self.traveller.start_path)
-        start_node = self._start_path_node(root, start_path_tree_str)
+        start_path_tree_str =  "/#" + str(self.read_start_dir)
+        start_node = MarkdownRenderer._find_node_by_fullpath(root, start_path_tree_str)
+        assert start_node is not None
         self.detach_from_parent(start_node)
         return start_node
 
     def run(self) -> None:
-        paths = list(self.traveller.run()) # all markdown paths discovered by traveller
+        paths: Iterable[Path] = self.find_markdowns()
         node = self.make_tree(paths) # internal tree
         print(RenderTree(node))
 
@@ -117,11 +109,11 @@ class BasicRenderer(RendererBase):
 
             # TODO create dirs, write squashed markdowns to pdf
             stream: BytesIO = self.engine.render(squashed)
-            self.engine.render_to_file(squashed, Path('dupa.pdf'))        
+            self.engine.render_to_file(squashed, Path('dupa.pdf'))   
+
 
 if __name__ == "__main__":
-    from wildland.md_pdf.traveller import MarkdownEverySubdir
-    start_dir: Path = Path.home()/"wildland/gitlab/cargo"
-    traveller = MarkdownEverySubdir(start_dir)
-    renderer = BasicRenderer(traveller)
+    read_start_dir = Path.home()/"wildland/gitlab/cargo"
+    write_start_dir = Path.home()/"wildland-client/dupa"
+    renderer = MarkdownRenderer(read_start_dir, write_start_dir)
     renderer.run()
